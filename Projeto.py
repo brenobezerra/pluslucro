be7623e69c9b35215bc350d13d88c701e7d9b4b4
brenobezerra <42545949+brenobezerra@users.noreply.github.com>
2025-07-30 11:45:51 -0300

    2 commit

--- Projeto.py
+++ Projeto.py
@@ -5,5 +5,9 @@ print("A soma é:", soma(5, 3))
 
 def divisao(a, b):
     return a / b
-
 print("A divisão é:", divisao(10, 2))
+
+def multiplicacao(a, b):
+    return a * b
+print("A multiplicação é:", multiplicacao(10, 3))
+
