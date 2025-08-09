ea3dd7e1fad0e5d9488904e6a50dd9c65b21ff20
brenobezerra <42545949+brenobezerra@users.noreply.github.com>
2025-08-09 09:58:00 -0300

    Adição Database url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://usuario:senha@localhost:5432/seubanco"
+DATABASE_URL = "postgresql://postgres:novasenha@localhost:5432/gastos_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
